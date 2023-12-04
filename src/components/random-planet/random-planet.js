b16276ff0ebebee57491a219cea02555b7bf9ae9
unknown <radmirsolo@gmail.com>
2023-12-05 02:17:06 +0600

    рефакторинг кода добавленине 2 новых кнопок работа с жизеннным циком

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
 
-import ErrorIndicator from "../error-indicator";
-import SwapiService from "../../services/swapi-service";
 import './random-planet.css';
-import Spinner from '../spinner'
+import ErrorIndicator from "../error-indicator";
+import SwapiService from '../../services/swapi-service';
+import Spinner from '../spinner';
+
 export default class RandomPlanet extends Component {
 
     swapiService = new SwapiService();
@@ -14,24 +15,23 @@ export default class RandomPlanet extends Component {
         error: false
     };
 
-    constructor() {
-        super();
+    componentDidMount() {
         this.updatePlanet();
+        this.interval = setInterval( this.updatePlanet, 60000 )
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval)
     }
 
     onPlanetLoaded = (planet) => {
         this.setState({
             planet,
-            loading: false
-        })
+            loading: false,
+            error : false
+        });
     };
 
-    updatePlanet() {
-        const id = Math.floor(Math.random() * 25 + 2);
-        this.swapiService.getPlanet(id)
-        .then(this.onPlanetLoaded)
-        .catch(this.onError)
-    }
     onError = (err) => {
         this.setState({
             error: true,
@@ -39,10 +39,18 @@ export default class RandomPlanet extends Component {
         })
     };
 
+    updatePlanet = () => {
+        const id = Math.floor( Math.random() * 17 ) + 3;
+
+        this.swapiService
+            .getPlanet(id)
+            .then(this.onPlanetLoaded)
+            .catch(this.onError);
+    };
 
     render() {
 
-        const { planet, loading, error} = this.state;
+        const {planet, loading, error} = this.state;
 
         const hasData = !(loading || error);
         const errorMessage = error ? <ErrorIndicator /> : null;
@@ -65,7 +73,7 @@ const PlanetView = ({planet}) => {
     return (
         <React.Fragment>
             <img className="planet-image"
-                src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} alt="planet"/>
+                src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
             <div>
                 <h4>{name}</h4>
                 <ul className="list-group list-group-flush">
@@ -84,5 +92,5 @@ const PlanetView = ({planet}) => {
                 </ul>
             </div>
         </React.Fragment>
-    );
+    )
 };
