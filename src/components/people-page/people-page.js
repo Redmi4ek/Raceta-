1a1b4bf53fbdee8ca33339f96dc7f1d3ae891c9d
redmi4ek <radmirsolo@gmail.com>
2023-12-05 12:02:51 +0600

    Hoc комопнент что то интересное

--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -1,43 +1,41 @@
 import React, {Component} from 'react';
 import './people-page.css';
 import ItemList from '../item-list';
-import PersonDetails from "../person-details";
-import ErrorIndicator from "../error-indicator";
+import ItemDetails from "../item-details";
 import SwapiService from "../../services/swapi-service";
 import Row from "../row";
-
+import ErrorBoundry from "../error-boundry";
 
 export default class PeoplePage extends Component {
     swapiService = new SwapiService();
 
     state = {
         selectedPerson: 3,
-        hasError: false
     };
 
-    componentDidCatch(error, errorInfo) {
-        this.setState({
-            hasError: true
-        })
-    }
 
     onPersonSelected = (selectedPerson) => {
         this.setState({ selectedPerson })
     };
-
+    
     render() {
-        if(this.state.hasError) return <ErrorIndicator />;
+        const itemList = (
+            <ItemList
+                onItemSelected={this.onPersonSelected}
+                getData={this.swapiService.getAllPeople}
+            >
+                { (i) => `${i.name} (${i.birthYear})` }
+            </ItemList>
+        );
 
-        const itemList = (<ItemList
-            onItemSelected={this.onPersonSelected}
-            getData={this.swapiService.getAllPeople}
-            renderItem={  ({ name, gender, birthYear }) => `${name} (${gender}, ${birthYear})`}
-        />);
-
-        const personDetails = <PersonDetails personId={this.state.selectedPerson} />                    
+        const itemDetails = (
+                <ErrorBoundry>
+                    <ItemDetails itemId={this.state.selectedPerson} />            </ErrorBoundry>
+            );
 
         return (
-          <Row left ={itemList} right={personDetails}/>
+            <Row left={itemList} right={itemDetails} />
+
         )
     }
 }
